cf019d8d8ea8a24fc5e44c0959ab88397986a381
nadhirfr <nadhir.fahrul.rozam@gmail.com>
2016-04-30 09:42:15 +0700

    menambahkan log.d pada onCreate onStart onPause onStop onDestroy, membuat form input text password dan umur, warna background, relative dan linear layout

--- app/src/main/java/id/rozam/olc1/MainActivity.java
+++ app/src/main/java/id/rozam/olc1/MainActivity.java
@@ -2,9 +2,14 @@ package id.rozam.olc1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private static final String TAG = "MainActivity-nya";
+    //private static final String TAG = this.getClass().getSimpleName();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
         TextView txtHello = (TextView) findViewById(R.id.txtHello);
         txtHello.setText("Ini ganti");
+    }
+
+    @Override
+    protected void onStart() {
+        int a = 1;
+        Log.d(TAG, "onStart = " + String.valueOf(a));
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "On resume terpanggil");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "On pause terpanggil");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: onStop terpanggil");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: onDestroy terpanggil");
+        super.onDestroy();
     }
 }
